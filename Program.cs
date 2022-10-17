9ee7a7d7bb9d5e78d592f849534329d2e24bba1d
Gayana-19 <64000886+Gayana-19@users.noreply.github.com>
2022-10-17 10:31:35 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,44 @@
-﻿// Main Program
-using employeees;
+﻿using oops1;
 
-class Program
+class program
 {
     static void Main(string[] args)
     {
-        // Creating Tech Employee Object and assigning values
-        TechEmployee emp = new TechEmployee();
-        emp.AcceptTechEmployee("Employee", 'M', "1/1/2001", 21,
-            "employee123@company.com", "passwd", "empid01",
-            "SDE", "Manager", 1);
-        Console.WriteLine("Employee Details\n");
-        emp.TechEmpDisp("empid01");
-
-        Console.WriteLine("\n");
-
-        // Create Manager Object and assigning values
-        Manager mgr = new Manager();
-        mgr.AcceptManager("Manager", 'F', "2/2/2000", 22,
-            "manager234@company.com", "passwd01", "mgrid01",
-            "Software Engineering", "Tech Lead", 8, 2);
-        Console.WriteLine("Manager Details\n");
-        mgr.ManagerDisp("mgrid01");
-
+        int ch;
+        do
+        {
+            Console.WriteLine("select any option");
+            server obj = new server();
+            obj.display();
+            Console.WriteLine("click 0 to exit\n click 1 to deposit \n click 2 to withdraw");
+            ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    obj.accdeposit();
+                    break;
+                case 2:
+                    obj.accwithdraw();
+                    break;
+                default:
+                    Console.WriteLine("invalid!");
+                    break;
+            }
+        }
+        while (ch! == 0);
     }
 }
+   
+
+
+            
+
+
+
+
+
+
+
+
+
+
